998056b98ac2ec03fbd2c52d33242212fd048840
antonio160484 <antoperez@live.com.mx>
2025-09-26 10:42:29 -0300

    Nuevas clases agregadas: Cliente y Voucher

---
+++ TPWeb_equipo-23A/negocio/ClienteNegocio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ClienteNegocio
+    {
+        public void agregar(Cliente nuevo) 
+        {
+        }
+    
+
+        public void modificar(Cliente nuevo)
+        {
+        }
+    }
+}
